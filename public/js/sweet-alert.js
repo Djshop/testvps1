781759fb26cc15e009b7344fadc33733b34f9d6e
YanisM1 <yanis.mengouchi1@gmail.com>
2022-07-14 13:59:29 +0200

    sweet alert + start mail

--- public/js/sweet-alert.js
+++ public/js/sweet-alert.js
@@ -4,7 +4,7 @@ function sweetAlertConnected() {
     
     swal({
       title: "Vous n'êtes pas connecté",
-      text: "Veuiller vous connecter à votre profil ou vous inscrire pour pouvoir effectuer une commande",
+      text: "Veuillez vous connecter à votre profil ou vous inscrire pour pouvoir effectuer une commande",
       icon: "warning",
       button: "Connexion",
     })
@@ -19,7 +19,7 @@ function sweetAlertVerified() {
     
     swal({
       title: "Votre compte n'est pas vérifié",
-      text: "Veuiller accéder au mail qui vous a été envoyé à l'inscription pour valider votre compte",
+      text: "Veuillez accéder au mail qui vous a été envoyé à l'inscription pour valider votre compte",
       icon: "warning",
       button: "Ok",
     })
